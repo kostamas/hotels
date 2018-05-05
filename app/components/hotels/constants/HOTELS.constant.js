30290108ee68cf3dad94c0279c0acc1d6870d52e
kostamas <kosta.masliansky@gmail.com>
2018-05-05 23:42:09 +0300

    satablize

--- app/components/hotels/constants/HOTELS.constant.js
+++ app/components/hotels/constants/HOTELS.constant.js
@@ -1,7 +1,7 @@
 angular.module('hotels')
     .constant('HOTEL', {
-        BRANDS_URL: 'https://jsonblob.com/api/jsonBlob/03b65292-2c22-11e7-ae4c-c56c53cfee28',
-        HOTELS_URL: 'https://jsonblob.com/api/jsonBlob/7a240de1-2c23-11e7-ae4c-c553a6f37282',
+        BRANDS_URL: 'https://jsonblob.com/api/jsonBlob/34322434-50a4-11e8-91fd-472a47502269',
+        HOTELS_URL: 'https://jsonblob.com/api/jsonBlob/23e4196b-50a4-11e8-91fd-d90877939b65',
         LOCAL_STORAGE_VISITED_ITEMS: 'visitedHotels',
         ALL_HOTELS_VIEW: 1,
         VISITED_HOTELS_VIEW: 2
